ec50d7813f70eb3945e437d2593797f4bb0dff21
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-05-21 12:24:12 -0700

    Forgot to push last night work
    added some style

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var today = new Date();
+var date = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate(); 
+document.getElementById("currentDate").innerHTML = date;
+ 
+ 
  $( function() {
     $( "#sortable" ).sortable();
   } );
