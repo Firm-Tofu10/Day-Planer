7e9e1e8c92e75063144721c5a04b33f3f4211d88
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-05-21 19:09:05 -0700

    Build 1.0
    Final build before turning the assignment in 5/21. By- Bryson Benson

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Work Day Scheduler Starter Code
 Bryson Benson
-To do:
-Save Events for each hour of the day.
-The starter code uses the Moment.js- https://momentjs.com/ library to work with date and time, but feel free to use a different JavaScript solution to handle this functionality since Moment.js
- since Moment.js is considered a "legacy" more about some alternative solutions- https://momentjs.com/docs/#/-project-status/
-Application deployed at live URL
+date of finished product- 5/21/2022
+
+This project was created using some provided starter code in the CSS and HTML however most od this was for style or linking Jquery,Bootstrap, and movment.js. (All the Javascript code was added to this project that was not an API call.)
+
+I injoyed the feedback loop of this assinment alot, it was realy cool to see this project interact in real time with the movment.js API.
+
+This project lets you plan your work day by letting you save your plan line by line with a time attached. It will then change the color of the line based on the time of day to let the user see iif they are meeting there own plan.
--- script.js
+++ script.js
@@ -3,9 +3,11 @@
 
 
 // document.getElementById("currentDay").textContent = date;
- setInterval(function(){
+ // (This was my first attempt at adding the time. Later I was told by a tuitor that this was good but movment.js would work slitly better for this project. This ended up being good advise as I saw the why of this while writing in JS.)
+
+setInterval(function(){
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
- },1000);
+ },1000); //This is calling the time every millisecond
 
 
  $(".btn").on("click",function(){
@@ -13,7 +15,7 @@ $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
    var currenthour = $(this).attr("id");
    console.log(userplan,currenthour);
    localStorage.setItem(currenthour,userplan);
- })
+ })//This funtion lets the user save there plan for the day to a certain time of day.
  var currentHour = moment().hours()
  for (let i = 6; i < 25; i++) {
    $("#"+ i).siblings(".textBox").val(localStorage.getItem(i))
@@ -23,9 +25,9 @@ $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
     $("#"+ i).siblings(".textBox").addClass("bg-success")
    }else{
     $("#"+ i).siblings(".textBox").addClass("bg-primary")
-   }
+   } //This is storing the users data to localStorage then calling that data after refreshing the page.
  };
 
- $( function() {
-    $( "#sortable" ).sortable();
-  } );
+ //$( function() {
+ //   $( "#sortable" ).sortable();
+ // } );
