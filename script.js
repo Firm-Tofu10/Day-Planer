09b8dcc4b0b4e37c77d03250a98950529b6786e9
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-05-21 18:07:51 -0700

    push 5/21

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-var today = new Date();
-var date = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate(); 
+// var today = new Date();
+// var date = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDate(); 
 
 
-document.getElementById("currentDate").innerHTML = date;
- 
+// document.getElementById("currentDay").textContent = date;
+ setInterval(function(){
+$("#currentDay").textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+ },1000);
+
+
+ $(".btn").on("click",function(){
+   var userplan = $(this).siblings(".textBox").val()
+   var currenthour = $(this).attr("id");
+   console.log(userplan,currenthour);
+ })
+
  $( function() {
     $( "#sortable" ).sortable();
   } );
